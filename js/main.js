9abe153366953733d30a1877dcad923d78600ab7
Alexander Pop <50931164+nekazsz@users.noreply.github.com>
2021-02-01 19:58:18 +0200

    develop in process

--- js/main.js
+++ js/main.js
@@ -1,20 +1,57 @@
 let adMessage = document.querySelector('.todo_btn'),
-    inputMessage = document.querySelector('.todo_in'),
+    input = document.querySelector('.todo_in'),
     messagesBody = document.querySelector('.todo_body');
 
 let mainArray = [];
 
+const updateLocalStorage = () => {
+    localStorage.setItem('todo', JSON.stringify(mainArray));
+}
+
+if (!localStorage.todo) {
+    mainArray = [];
+} else {
+    mainArray = JSON.parse(localStorage.getItem('todo'));
+    adTodoli();
+}
+
 function eventObject() {
     let todoObject = {
-        message: 'inputMessage',
-        important: false,
-        perform: false,
-        hidden: false
+        message: input.value,
+        perform: true,
+        hidden: false,
+        important: false
     };
+    mainArray.push(todoObject);
+    updateLocalStorage();
+    adTodoli();
+    input.value = '';
 }
 
 adMessage.addEventListener('click', function () {
-    mainArray += todoObject;
+    eventObject();
+    console.log(mainArray);
 });
+
+
+function adTodoli() {
+    let message = '';
+
+    mainArray.forEach(function (item, i) {
+        message +=
+            `<li class="liItem ${item.important} ${item.perform} ${item.hidden}">
+                    <span class="${item.perform ? 'performTaskGreen' : 'performTaskRed'}">${item.perform ? 'Ok' : ''}</span>
+                    <span class="divMessage"><p>${item.message}</p></span>
+                    <span class="btnWr">
+                        <button type="button" class="btnHidden">H</button>
+                        <button type="button" class="btnImportant">!</button>
+                        <button type="button" class="delTask">X</button>
+                    </span>
+             </li>`;
+        messagesBody.innerHTML = message;
+    })
+}
+
+
 
 
